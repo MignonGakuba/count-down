7fe929ad4b0319e85696be4afe05e8be040741a9
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-10-31 22:03:04 +0100

    added

--- count_down/src/App.js
+++ count_down/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { add } from 'date-fns';
+
+import { Ticker } from 'components/Ticker';
+
+import style from './App.module.scss';
+
+// Defaults to 6 days, 22 hours, 40 minutes and 0 seconds from now in your timezone.
+const futureDate = add(new Date(), {
+  days: 6,
+  hours: 22,
+  minutes: 40
+});
 
 function App() {
+  const [tickerVisible, setTickerVisible] = useState(false);
+  const tickerEl = tickerVisible ? <Ticker futureDate={futureDate} /> : null;
+  const toggleText = tickerVisible ? 'Hide Countdown': 'Show Countdown';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={ style.pageContainer }>
+      <h1>Count Event Elden Ring!!</h1>
+      <p>Come check out our great event. It will be lots of fun.</p>
+      { tickerEl }
+      <button 
+        className={ style.toggleButton } 
+        onClick={() => setTickerVisible(prev => !prev) }
+      >
+        { toggleText }
+      </button>
+    </div>  
   );
 }
 
