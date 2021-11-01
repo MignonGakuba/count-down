0766734348697d7ee9eaa0398376e4949d36349d
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-11-01 22:49:52 +0100

    Change the background image

--- count_down/src/App.js
+++ count_down/src/App.js
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import { add } from 'date-fns';
-
 import { Ticker } from './components/Ticker/Ticker.js';
-
 import style from './App.module.scss';
 
+
+
+
+
 // Defaults to 6 days, 22 hours, 40 minutes and 0 seconds from now in your timezone.
+// Set the 25 februari Elden Ring
 const futureDate = add(new Date(), {
   months:1,
   weeks:1,
@@ -14,6 +17,7 @@ const futureDate = add(new Date(), {
   minutes: 30
 });
 
+
 function App() {
 
   const [tickerVisible, setTickerVisible] = useState(false);
@@ -21,17 +25,19 @@ function App() {
   const toggleText = tickerVisible ? 'Hide Countdown': 'Show Countdown';
 
   return (
-    <div className={ style.pageContainer }>
-      <h1>Join Our Event</h1>
-      <p>Come check out our great event. It will be lots of fun.</p>
-      { tickerEl }
-      <button 
-        className={ style.toggleButton } 
-        onClick={() => setTickerVisible(prev => !prev) }
-      >
-        { toggleText }
-      </button>
-    </div>  
+    <div className = {style.backgroundImage}>
+        <div className={ style.pageContainer }>
+          <h1>Join Our Event</h1>
+          <p>Come check out our great event. It will be lots of fun.</p>
+            { tickerEl }
+            <button 
+              className={ style.toggleButton } 
+              onClick={() => setTickerVisible(prev => !prev) }
+            >
+            { toggleText }
+            </button>
+        </div>  
+    </div>
   );
 }
 
